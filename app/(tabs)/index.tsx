e56c984cd9cab1933172f5deaeed150b0d0fcd54
Lara Segatello Marquez <lara.marquez@aluno.senai.br>
2026-02-12 10:00:25 -0300

    ultimas mudancas finais

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -13,13 +13,14 @@ const NoticiasScreen = () => {
           <Text style={Styles.Cabecalho}>Contato</Text>
           <Text style={Styles.Cabecalho}>Notícias</Text>
         </View>
-        <View style={Styles.oneNoticia}>
+        <View style={Styles.Noticia}>
           <Image
             source={require("../assets/img/IMG_5496-1024x761.jpg")}
-            style={Styles.imgNoticias}>
+            style={Styles.imgNoticias}
+            resizeMode="cover">
 
           </Image>
-          <Text style={Styles.textNoticias}> Novo vazamento na Baía de Guanabara abre caminho para canal de
+          <Text style={Styles.textNoticias}>Novo vazamento na Baía de Guanabara abre caminho para canal de
             denúncia online</Text>
           <Text style={Styles.Noticiap}>Um novo vazamento de óleo ocorrido na tarde deste domingo, em São
             Gonçalo (RJ), trouxe à tona a necessidade de um canal participativo
@@ -27,25 +28,27 @@ const NoticiasScreen = () => {
             óleo nas baías fluminenses (Guanabara, Sepetiba e da Ilha Grande).</Text>
         </View>
 
-        <View style={Styles.twoNoticia}>
+        <View style={Styles.Noticia}>
           <Image
             source={require("../assets/img/1_enem-1-60987139.webp")}
-            style={Styles.imgNoticias}>
+            style={Styles.imgNoticias}
+            resizeMode="cover">
 
           </Image>
-          <Text style={Styles.textNoticias}> MEC aciona PF e anula três questões do segundo dia do Enem</Text>
+          <Text style={Styles.textNoticias}>MEC aciona PF e anula três questões do segundo dia do Enem</Text>
           <Text style={Styles.Noticiap}> Medida foi adotada após relatos nas redes sociais sugerirem a
             antecipação de questões semelhantes às aplicadas no segundo dia do
             exame.</Text>
         </View>
 
-        <View style={Styles.threeNoticia}>
+        <View style={Styles.Noticia}>
           <Image
-            source={require("../assets/img/paqueta-censo-2022090620210064.webp")} 
-            style={Styles.imgNoticias}>
+            source={require("../assets/img/paqueta-censo-2022090620210064.webp")}
+            style={Styles.imgNoticias}
+            resizeMode="cover">
 
           </Image>
-          <Text style={Styles.textNoticias}> Novo vazamento na Baía de Guanabara abre caminho para canal de
+          <Text style={Styles.textNoticias}>Novo vazamento na Baía de Guanabara abre caminho para canal de
             denúncia online</Text>
           <Text style={Styles.Noticiap}>Um novo vazamento de óleo ocorrido na tarde deste domingo, em São
             Gonçalo (RJ), trouxe à tona a necessidade de um canal participativo
@@ -62,83 +65,82 @@ const NoticiasScreen = () => {
 
 
 const Styles = StyleSheet.create({
-  body:{
-     backgroundColor: "light grey",
-     fontFamily: "Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif",
-     margin: 0,
-     padding: 0,
-     boxSizing: "border-box",
-     lineHeight: 1.6
-    },
-
-  header:{
-     backgroundColor: "red",
-     color: "write",
-     padding: 5,
-     boxShadow:'0 4px 6px rgba(0,0,0,0.1)',
-     top: 0,
-     zIndex: 1000,
-     flexDirection:"row",
+  body: {
+    backgroundColor: "light grey",
+    // fontFamily: "Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif",
+    // margin: 0,
+    // padding: 0,
+    // boxSizing: "border-box",
+    // lineHeight: 1.6
   },
 
-  Cabecalho:{
-     backgroundColor: "light grey",
-     maxWidth: 1200,
-     margin: 0,
-     padding: 20,
-     
-     justifyContent: "space-between",
-     alignItems: "center",
-     flexWrap: "wrap" 
+  header: {
+    backgroundColor: "red",
+    color: "write",
+    // padding: 5,
+    boxShadow: '0 4px 6px rgba(0,0,0,0.1)',
+    top: 0,
+    // zIndex: 1000,
+    flexDirection: "row"
   },
 
-  oneNoticia:{
-     backgroundColor: "white",
-     padding: 15,
-     borderRadius: 8,
-     boxShadow:'0 2px 4px rgba(243, 7, 7, 0.05',
-     textAlign:"justify"
+  Cabecalho: {
+    backgroundColor: "light grey",
+    // maxWidth: 1200,
+    margin: 0,
+    padding: 20,
+    justifyContent: "space-between",
+    alignItems: "center",
+    // flexWrap: "wrap"
   },
 
-  textNoticias:{
-    fontWeight:'bold',
-    fontSize: 1.3,
-    color: "Red"
+  Noticia: {
+    backgroundColor: "white",
+    paddingBottom: 15,
+    borderRadius: 12,
+    boxShadow: '0 2px 4px rgba(243, 7, 7, 0.05',
+    textAlign: "justify",
+    // maxWidth: 900,
+    alignSelf: 'center',
+    marginBottom: 20,
+    elevation: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.8
+  },
+
+  textNoticias: {
+    fontWeight: 'bold',
+    color: "Red",
+    marginTop: 10,
+    paddingHorizontal: 12
   },
 
   Noticiap: {
-    fontSize: 0.9,
-    color:"grey",
-    marginBottom: 0
+    color: "grey",
+    marginBottom: 0,
+    paddingHorizontal: 12,
   },
 
-  twoNoticia:{
-     backgroundColor: "white",
-     padding: 15,
-     borderRadius: 8,
-     boxShadow:'0 2px 4px rgba(243, 7, 7, 0.05',
-     textAlign:"justify"
-
+  footer: {
+    backgroundColor: "red",
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 15,
+    width: '100%'
   },
 
-  threeNoticia:{
-    backgroundColor: "light grey"
+  textfooter: {
+    color: "black",
+    fontSize: 15
   },
 
-  footer:{
-    backgroundColor: "light grey"
-  },
-
-  textfooter:{
-    color: "light grey"
-  },
-
-  imgNoticias :{
-    width: 100,
-    height: 180,
-    objectFit: "cover",
-    borderRadius: 6,
-    marginBottom: 10
+  imgNoticias: {
+    width: '100%',
+    height: 200,
+    borderTopLeftRadius: 12,
+    borderTopRightRadius: 12
   }
 
 });
